a39a4d8e6a41657bd423fe97531424518d9a6eab
Luis Barrios <20141112@lamolina.edu.pe>
2022-01-15 02:04:50 -0500

    plots
    some density, histogram, bar, boxplot, violin,etc worked with biologic data

---
+++ paper_work.R
@@ -0,0 +1,181 @@
+######################################
+########################################
+#               data micro             #
+########################################
+########################################
+# install.packages("ggpubr")
+library(psych)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(ggplot2)
+library(readxl)
+library(ggpubr)
+
+#------------------------------------------------------------#
+#------------------ SHANNON INDEX DATABASE ------------------#
+#------------------------------------------------------------#
+
+datamicro<-read_xlsx("C:\\Users\\LBarrios\\Documents\\proyectocolombia.xlsx")
+# DESCRIPTIVE STATS
+# str(datamicro)
+# describe(datamicro)
+# WORKING/formatting the data
+datamicro$Shannon<-paste0(substr(datamicro$Shannon,start = 1,stop = 1),".",substr(datamicro$Shannon,start = 2,stop = 4))
+datamicro$Shannon<-as.numeric(datamicro$Shannon)
+datamicro$Shannon<-round(datamicro$Shannon,digits = 2)
+# converting from number to labels in sample type
+datamicro$`Sample Type`<-ifelse( datamicro$`Sample Type`==3,"BAL",ifelse(datamicro$`Sample Type`==4,"Saline from BAL","Induced Sputum"))
+# converting from number to labels in time point
+datamicro$`Time Point`<-ifelse(datamicro$`Time Point`==0,"Admission",ifelse(datamicro$`Time Point`==4,"Follow-up (6 months)","ND") )
+
+# scaleFUN <- function(x) sprintf("%.1f", x)
+# +scale_y_continuous(labels=scaleFUN)
+# plotting with ggplot2
+# los ggsave son para exportarlo en un formato de alta calidad
+
+#--------------------- BOXPLOT + VIOLIN ENTRE GRUPOS ---------------------#
+datamicro %>%
+  ggplot(mapping = aes(x=Group,y=Shannon, fill=Group))+
+  geom_violin(trim = FALSE, alpha=0.5)+
+  geom_boxplot(width=0.3)+ labs(y="Shannon Index",x="")+theme_classic()
+
+ggsave(path = "C:\\Users\\LBarrios\\Desktop\\Articulo_micro_colombia\\plots",
+       width = 7, height = 5, device='tiff', dpi=300,filename = "shannonbygroup.png")
+
+#--------------------- BOXPLOT + VIOLIN ENTRE SAMPLE TYPE ---------------------#
+
+datamicro %>%
+  ggplot(mapping = aes(x=factor(`Sample Type`),y=Shannon,fill=factor(`Sample Type`)))+
+  geom_violin(trim = FALSE, alpha=0.5)+
+  geom_boxplot(width=0.3)+
+  labs(x="",y= "Shannon Index")+
+  guides(fill=guide_legend(title="Sample Type"))+theme_classic()+xlim("BAL","Induced Sputum")+
+  scale_fill_discrete(breaks=c("BAL","Induced Sputum"))
+
+ggsave(path = "C:\\Users\\LBarrios\\Desktop\\Articulo_micro_colombia\\plots",
+       width = 7, height = 5, device='tiff', dpi=300,filename = "shannonbysampletype.png")
+#--------------------- BOXPLOT + VIOLIN ENTRE TIME POINT ---------------------#
+datamicro %>%
+  ggplot(mapping = aes(x=`Time Point`,y=Shannon,fill=`Time Point`))+
+  geom_violin(trim = FALSE, alpha=0.5,color="black")+
+  geom_boxplot(width=0.13)+
+  labs(x="",y= "Shannon Index")+
+  guides(fill=guide_legend(title="Time Point"))+theme_classic()
+
+ggsave(path = "C:\\Users\\LBarrios\\Desktop\\Articulo_micro_colombia\\plots",
+       width = 7, height = 5, device='tiff', dpi=300,filename = "shannonbytimepoint.png")
+# trying new things
+# DENSITY FUNCTIONS
+a<-datamicro %>%
+  ggplot(aes(x=Shannon, fill=Group))+
+  geom_density(alpha=0.6, color="black")+
+  labs(x="Shannon Index",y="Density")+
+  guides(fill=guide_legend(title="Group"))+theme_classic()+geom_vline(aes(xintercept=mean(Shannon)),
+                                                                      color="black", linetype="dashed", size=0.5)
+
+b<-datamicro %>%
+  ggplot(aes(x=Shannon, fill=`Sample Type`))+
+  geom_density(alpha=0.6, color="black")+
+  labs(x="Shannon Index",y="Density")+
+  guides(fill=guide_legend(title="Sample Type"))+theme_classic()+geom_vline(aes(xintercept=mean(Shannon)),
+                                                                      color="black", linetype="dashed", size=0.5)+
+  scale_fill_discrete(breaks=c("BAL","Induced Sputum"))
+
+c<-datamicro %>%
+  ggplot(aes(x=Shannon, fill=`Time Point`))+
+  geom_density(alpha=0.6, color="black")+
+  labs(x="Shannon Index",y="Density")+
+  guides(fill=guide_legend(title="Time Point"))+theme_classic()+geom_vline(aes(xintercept=mean(Shannon)),
+                                                                            color="black", linetype="dashed", size=0.5)
+# CON ggarrange podemos unir los gráficos en uno solo
+figure<-ggarrange(c, ggarrange(b, a, ncol = 2, labels = c("B", "C"),legend = "bottom"), 
+                  nrow = 2, 
+                  labels = "A"       # Label of the line plot
+) 
+figure
+
+ggsave(path = "C:\\Users\\LBarrios\\Desktop\\Articulo_micro_colombia\\plots",
+       width = 7, height = 7, device='tiff', dpi=300,filename = "separated_densitybygroup_timepoint_sampletype.png")
+#
+datamicro %>%
+  ggplot(aes(x=Shannon))+
+  geom_density(alpha=0.6, color="black")+
+  labs()+
+  guides(fill=guide_legend(title="Time Point"))+theme_classic()+geom_vline(aes(xintercept=mean(Shannon)),
+                                                                           color="black", linetype="dashed", size=0.5)+
+  facet_grid(Group~ .)
+
+# some histograms
+datamicro %>% ggplot(aes(x=Shannon))+
+  geom_histogram(color="black",fill="white", alpha=0.5, position="identity")+theme_classic()+
+  facet_grid(`Sample Type`~ .)+
+  geom_density(alpha=0.6)
+
+
+#barplots
+datamicro %>%filter(`Sample Type`!="Saline from BAL") %>% 
+  ggplot(aes(x=Group,y=Shannon,fill=`Time Point`))+
+  geom_bar(stat = "identity")+theme_classic()+facet_grid(`Sample Type`~ .)
+
+# Density plot with facet grid, 3 variables in one plot
+datamicro %>%filter(`Sample Type`!="Saline from BAL") %>% 
+  ggplot(aes(x=Shannon, fill=Group))+
+  geom_density(alpha=0.6, color="black")+
+  labs(x="Shannon Index",y="Density")+
+  guides(fill=guide_legend(title="Group"))+theme_classic()+
+  facet_grid(vars(`Sample Type`),vars(`Time Point`))
+
+ggsave(path = "C:\\Users\\LBarrios\\Desktop\\Articulo_micro_colombia\\plots",
+       width = 7, height = 5, device='tiff', dpi=300,filename = "densitybygroup_timepoint_sampletype.png")
+# boxplots mas violines con p-values
+#group
+datamicro %>%
+  ggplot(mapping = aes(x=Group,y=Shannon, fill=Group))+
+  geom_violin(trim = FALSE, alpha=0.5)+
+  geom_boxplot(width=0.3)+ labs(y="Shannon Index",x="")+theme_classic()+
+  geom_segment(aes(x = 1, y = 6.4, xend = 2, yend = 6.4))+
+  geom_segment(aes(x = 1, y = 6.3, xend = 1, yend = 6.4))+
+  geom_segment(aes(x = 2, y = 6.3, xend = 2, yend = 6.4))+
+  geom_segment(aes(x = 2.05, y = 6.3, xend = 3, yend = 6.3))+
+  geom_segment(aes(x = 2.05, y = 6.2, xend = 2.05, yend = 6.3))+
+  geom_segment(aes(x = 3, y = 6.2, xend = 3, yend = 6.3))+
+  geom_segment(aes(x = 1, y = 6.9, xend = 3, yend = 6.9))+
+  geom_segment(aes(x = 1, y = 6.9, xend = 1, yend = 6.8))+
+  geom_segment(aes(x = 3, y = 6.9, xend = 3, yend = 6.8))+
+  geom_text(aes(x=1.5,y=6.6),label="0.07")+
+  geom_text(aes(x=2.5,y=6.5),label="0.28")+
+  geom_text(aes(x=2,y=7.1),label="0.0039")
+
+ggsave(path = "C:\\Users\\LBarrios\\Desktop\\Articulo_micro_colombia\\plots",
+       width = 7, height = 5, device='tiff', dpi=300,filename = "shannonbygrouppvalues.png")
+#sample type
+
+datamicro %>%
+  ggplot(mapping = aes(x=`Sample Type`,y=Shannon,fill=`Sample Type`))+
+  geom_violin(trim = FALSE, alpha=0.5)+
+  geom_boxplot(width=0.3)+
+  labs(x="",y= "Shannon Index")+
+  guides(fill=guide_legend(title="Sample Type"))+theme_classic()+xlim("BAL","Induced Sputum")+
+  geom_segment(aes(x = 1, y = 6.6, xend = 2, yend = 6.6))+
+  geom_segment(aes(x = 1, y = 6.5, xend = 1, yend = 6.6))+
+  geom_segment(aes(x = 2, y = 6.5, xend = 2, yend = 6.6))+
+  geom_text(aes(x=1.5,y=6.8),label="0.09")+
+  scale_fill_discrete(breaks=c("BAL","Induced Sputum"))
+
+ggsave(path = "C:\\Users\\LBarrios\\Desktop\\Articulo_micro_colombia\\plots",
+       width = 7, height = 5, device='tiff', dpi=300,filename = "shannonbysampletypepvalues.png")
+#TIME POINT
+datamicro %>%
+  ggplot(mapping = aes(x=`Time Point`,y=Shannon,fill=`Time Point`))+
+  geom_violin(trim = FALSE, alpha=0.5,color="black")+
+  geom_boxplot(width=0.13)+
+  labs(x="",y= "Shannon Index")+
+  guides(fill=guide_legend(title="Time Point"))+theme_classic()+
+  geom_segment(aes(x = 1, y = 6.6, xend = 2, yend = 6.6))+
+  geom_segment(aes(x = 1, y = 6.5, xend = 1, yend = 6.6))+
+  geom_segment(aes(x = 2, y = 6.5, xend = 2, yend = 6.6))+
+  geom_text(aes(x=1.5,y=6.8),label="0.02")
+
+ggsave(path = "C:\\Users\\LBarrios\\Desktop\\Articulo_micro_colombia\\plots",
+       width = 7, height = 5, device='tiff', dpi=300,filename = "shannonbytimepointpvalues.png")
